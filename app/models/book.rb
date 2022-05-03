597b975d7fe18f6e178cd57a90141978717995d6
EC2 Default User <ec2-user@ip-172-31-11-11.ec2.internal>
2022-05-03 01:19:50 +0000

    応用課題５

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,6 @@ class Book < ApplicationRecord
   def favorited?(user)
     favorites.where(user_id: user.id).exists?
   end
+  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,18 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
