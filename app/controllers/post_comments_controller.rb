dc3666677426d13b977fadab388d5f250eff61d7
EC2 Default User <ec2-user@ip-172-31-11-11.ec2.internal>
2022-05-07 05:31:51 +0000

    Finish6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,18 +1,16 @@
 class PostCommentsController < ApplicationController
 
   def create
-    
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @comment = current_user.post_comments.new(post_comment_params)
-    @comment.book_id = @book.id
+    @comment.book_id = book.id
     @comment.save
-
   end
 
   def destroy
-  
-    PostComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
-    @book = Book.find(params[:book_id])
+    @books = Book.all
+    @comment = PostComment.find_by(id: params[:id],book_id: params[:book_id])
+    @comment.destroy
   end
 
   private
